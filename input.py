db56b22b26f87c55612d8f5fbff4193a71efb482
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2022-10-03 22:46:04 +0300

    Update input.py

--- input.py
+++ input.py
@@ -2,5 +2,6 @@ name=str(input("What is your name:"))
 age=int(input("How old are you:"))
 age+=1
 print("Your name is "+name)
+#concantenate the age variable with the string
 print(name+", You are "+str(age)+" years old")
 print(f"{name} you are {age} years old ")
