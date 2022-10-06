bd7e78e9c523c71fdfaa9a04ec208403a287acb8
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2022-10-07 01:28:17 +0300

    Update ifstatement.py

--- ifstatement.py
+++ ifstatement.py
@@ -3,4 +3,5 @@ age=int(input("Please enter your age:"))
 
 #This can be edited as:
 #print("Your age will be"+ age +"years after 10 years")
+#What I prefer most
 print(f"Your age will be {age+=10}yrs after 10 years")
