9cf16db3d67ab7aac8172875a6977759b5460f6c
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2022-10-10 16:42:56 +0300

    Update input.py

--- input.py
+++ input.py
@@ -1,5 +1,6 @@
 name=str(input("What is your name:"))
 age=int(input("How old are you:"))
+#increasing the age variable
 age+=1
 print("Your name is "+name)
 #concantenate the age variable with the string
