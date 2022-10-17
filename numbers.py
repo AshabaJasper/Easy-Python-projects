7bdbb75f9fd5bc89d91924312fd0f4486358a567
Ashaba <ashabajasper@gmail.com>
2022-10-17 11:17:28 +0300

    Added a few comments to the math module

--- numbers.py
+++ numbers.py
@@ -15,6 +15,7 @@ print(math.pi)
 print(math.ceil(pi))
 #absolute value of pi
 print(abs(pi)) #tells you how far a number is from zero
+#raising a number to its power
 print(pow(2,3))
 
 print(max(a,b,c))
