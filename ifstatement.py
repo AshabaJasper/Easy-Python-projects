32f9da617560bb700b6e7fdd669af7862b887d89
Ashaba <ashabajasper@gmail.com>
2022-10-19 15:03:20 +0300

    Update ifstatement.py

--- ifstatement.py
+++ ifstatement.py
@@ -1,7 +1,8 @@
-#initialize teh age input
-age=int(input("Please enter your age:"))
+name=str(input("What is your name:"))
+age=int(input("How are old you?:"))
 
-#This can be edited as:
-#print("Your age will be"+ age +"years after 10 years")
-#What I prefer most 
-print(f"Your age will be {age} yrs after 10 years")
+while not False:
+    if name:
+        print("Please enter your name")
+    else:
+        print(f"Welcome {name} you are {age} years old")    
