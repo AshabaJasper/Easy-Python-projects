71bb782063cff70ec40669a8f07748236ac84c35
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-03-16 23:22:26 +0300

    Update busbooking.py

--- busbooking.py
+++ busbooking.py
@@ -37,6 +37,8 @@ def main():
     print("-----------menu-----------\n")#this prints the menu of the bus company to the console
     print("1.Bus booking status\n2.Bus booking\n3.Booking payment\n4.Booking report\n5.Sign out")
         
+
+        #next step
     choice=int(input("enter your choice"))
     while choice==4 or choice==1:#repeats the statement when wrong credentials are entered
         choice=int(input("please enter 2,3,5"))
