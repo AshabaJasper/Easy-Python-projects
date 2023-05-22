e5ae94cbf0264d378d39a8f30c880b9474f26e78
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-05-22 07:53:07 +0300

    Update

--- Python Projects/calculator_app/add.py
+++ Python Projects/calculator_app/add.py
@@ -4,3 +4,7 @@ def add_two_numbers(num1, num2):
 print(add_two_numbers(5, 7))  # Output: 12
 
 #Adding numbers in a list
+def add_multiple_numbers(numbers):
+    return sum(numbers)
+
+print(add_multiple_numbers([5, 7, 9, 10]))  # Output: 31
