63ae3dcc6f3b50d02f394378938e919be41b36df
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-05-22 23:05:08 +0300

    Update

---
+++ Python Projects/Beginner_Projects/Thermometer.py
@@ -0,0 +1,15 @@
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
+
+temp = float(input("Enter the temperature: "))
+unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
+
+if unit.upper() == "C":
+    print("Temperature in Fahrenheit:", celsius_to_fahrenheit(temp))
+elif unit.upper() == "F":
+    print("Temperature in Celsius:", fahrenheit_to_celsius(temp))
+else:
+    print("Please enter correct unit (C/F)")
